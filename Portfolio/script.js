8b229fe9bd8ecda38799fe5cdeb98163eb09d1cf
Jesse Smart <jsmartdev@protonmail.com>
2024-04-01 17:32:32 -0400

    changed dropdown to enter/exit from left of screen

--- Portfolio/script.js
+++ Portfolio/script.js
@@ -1,7 +1,9 @@
 const hamburger = document.querySelector(".hamburger-menu");
 const navMenu = document.querySelector(".nav-menu");
+const navBar = document.querySelector(".nav-bar");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
+  navBar.classList.toggle("active");
 })
